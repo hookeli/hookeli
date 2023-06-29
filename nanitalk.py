7d179d936f1b316db330500b619fb9d3f0a46c05
lizhihong0817 <61351348+lizhihong0817@users.noreply.github.com>
2023-06-29 22:44:11 +0800

    Add files via upload

--- nanitalk.py
+++ nanitalk.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title('纳尼说Nanitalk')
 st.header('呆鸟日记') 
-st.image('/Users/apple/Desktop/AI/spider/nanitalk/IMG_3115.heic')
+st.image('/Users/apple/Desktop/AI/spider/nanitalk/IMG_2653.jpeg')
 st.markdown('这是一部超级有趣的动画电影......')
 
 st.header('纳尼说') 
@@ -16,7 +16,7 @@ st.markdown('这是一部超级有趣的动画电影......')
 
 option = st.selectbox('选择一个漫画:', ['纳尼说Nanitalk', '呆鸟日记', '虎克虎克！']) 
 if option == '纳尼说Nanitalk':
-   st.image('/Users/apple/Desktop/AI/spider/nanitalk/IMG_3115.heic')
+   st.image('/Users/apple/Desktop/AI/spider/nanitalk/IMG_2653.jpeg')
 elif option == '呆鸟日记':
    st.image('/Users/apple/Desktop/AI/spider/nanitalk/IMG_2653.jpeg')
 else:
