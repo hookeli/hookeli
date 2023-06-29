51a2cd8b8d9ecbf0f237cac1a4d6b69e3d3c2910
lizhihong0817 <61351348+lizhihong0817@users.noreply.github.com>
2023-06-29 22:39:49 +0800

    Add files via upload

--- nanitalk.py
+++ nanitalk.py
@@ -2,22 +2,22 @@ import streamlit as st
 
 st.title('纳尼说Nanitalk')
 st.header('呆鸟日记') 
-st.image('/Users/apple/Documents/绘画手稿/春游日.jpg')
+st.image('/Users/apple/Desktop/AI/spider/nanitalk/IMG_3115.heic')
 st.markdown('这是一部超级有趣的动画电影......')
 
 st.header('纳尼说') 
-st.image('/Users/apple/Documents/绘画手稿/0316小浣熊.jpg')
+st.image('/Users/apple/Desktop/AI/spider/nanitalk/IMG_2653.jpeg')
 st.markdown('这是一部超级有趣的动画电影......')
 
 st.header('虎克虎克！') 
-st.image('/Users/apple/Documents/绘画手稿/6541647874457_.pic.jpg')
+st.image('/Users/apple/Desktop/AI/spider/nanitalk/0316小浣熊.jpg')
 st.markdown('这是一部超级有趣的动画电影......')
 
 
 option = st.selectbox('选择一个漫画:', ['纳尼说Nanitalk', '呆鸟日记', '虎克虎克！']) 
 if option == '纳尼说Nanitalk':
-   st.image('/Users/apple/Documents/绘画手稿/春游日.jpg')
+   st.image('/Users/apple/Desktop/AI/spider/nanitalk/IMG_3115.heic')
 elif option == '呆鸟日记':
-   st.image('/Users/apple/Documents/绘画手稿/0316小浣熊.jpg')
+   st.image('/Users/apple/Desktop/AI/spider/nanitalk/IMG_2653.jpeg')
 else:
-    st.image('/Users/apple/Documents/绘画手稿/6541647874457_.pic.jpg')
+    st.image('/Users/apple/Desktop/AI/spider/nanitalk/0316小浣熊.jpg')
